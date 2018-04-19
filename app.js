fc0d08a21a9904e5be29a3c80f8df7d866b13cc0
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-04-19 11:12:05 -0400

    added use

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
 
-
-
-
-
-
-
+ 
+app.use(function(req, res, next){
+    console.log(req.method, req.path, res.statusCode);
+    next();
+});
 
 app.get('/', function(req, res){
     res.send('server listening');
 }); 
+
+app.get('/news', function(req, res){
+    res.send('this is the news');
+});
+
+
+
 
 app.listen(3000);
 
