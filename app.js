55f349e9eb76267fbc780846e5d897dbb61fda47
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-04-19 10:32:09 -0400

    listening and responding

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+app.get('/', function(req, res){
+    res.send('server listening');
+}); 
+
+app.listen(3000);
+
+
+//console.log('server listening');
