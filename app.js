b27555f7d13c4cafb0a612137dca16d9ead9d183
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-04-19 12:25:43 -0400

    got nunjucks up and running

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks');
+
+let locals = {
+  title: 'An example',
+  people: [
+    {name: 'Gandalf'},
+    {name: 'Frodo'},
+    {name: 'Hermione'}
+  ]
+}
+
+app.set('view engine', 'html'); // have res.render work with html files
+
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', { noCache: true}); // point nunjucks to the proper directory for templates
+
+// nunjucks.configure('views', { autoescape: true, express: app});
+// nunjucks.render('index.html', locals, function(err, output){
+//   if (err) throw err;
+//   console.log(output);
+// });
 
 
 app.use(function(req, res, next){
@@ -8,17 +29,11 @@ app.use(function(req, res, next){
 });
 
 app.get('/', function(req, res){
-    res.send('server listening');
+    res.render('index.html', locals);
 });
 
 app.get('/news', function(req, res){
     res.send('this is the news');
 });
 
-
-
-
 app.listen(3000);
-
-
-console.log('doli is here');
